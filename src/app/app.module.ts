d281fd078d227cb2d2bab5d7eaafb83b4a1fc871
Gurnoor Deol <gurnoor.deol@forgeahead.io>
2023-01-05 22:35:55 -0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+
+
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AuthComponent } from "./auth/auth.component";
+
+import { AlertComponent } from "./shared/alert/alert.component";
+
+import { RecipesModule } from "./recipes/recipes.module";
+import { ShoppingListModule } from "./shopping-list/shopping-list.module";
+import { SharedModule } from "./shared/shared.module";
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RecipesModule,
+    ShoppingListModule,
+    SharedModule,
+  ],
+
+  bootstrap: [AppComponent],
+  entryComponents: [AlertComponent],
+})
+export class AppModule { }
