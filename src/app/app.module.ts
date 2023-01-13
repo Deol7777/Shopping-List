fdd6e941f1d66980db10d743fe5904789bdbc652
Gurnoor Deol <gurnoor.deol@forgeahead.io>
2023-01-13 15:37:08 -0800

    Everything working uptill this point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,10 @@ import { HeaderComponent } from "./header/header.component";
 
 import { AppRoutingModule } from "./app-routing.module";
 
-import { AuthComponent } from "./auth/auth.component";
 
 import { AlertComponent } from "./shared/alert/alert.component";
-
-import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
-import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -26,11 +22,9 @@ import { AuthModule } from "./auth/auth.module";
 
     HttpClientModule,
     AppRoutingModule,
-    //RecipesModule,
-    ShoppingListModule,
     SharedModule,
     CoreModule,
-    AuthModule
+
   ],
 
   bootstrap: [AppComponent],
